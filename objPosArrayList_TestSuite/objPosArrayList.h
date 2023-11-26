cec30bf081bf5105e6506f89c130fb60521020a9
Ting Meng <mengt7@mcmaster.ca>
2023-11-25 19:12:54 -0500

    2A

--- objPosArrayList_TestSuite/objPosArrayList.h
+++ objPosArrayList_TestSuite/objPosArrayList.h
@@ -2,29 +2,33 @@
 #define OBJPOS_ARRAYLIST_H
 
 #define ARRAY_MAX_CAP 200
-
+#include <iostream>
 #include "objPos.h"
 
-class objPosArrayList
-{
+class objPosArrayList{
     private:
         objPos* aList;
-        int sizeList;
-        int sizeArray;
-
+        int sizeList;  //num of elements
+        int sizeArray;  //max size
+ 
     public:
         objPosArrayList();
         ~objPosArrayList();
+        objPosArrayList(const objPosArrayList &l);//deep copy
 
+    //getter
         int getSize();
-        void insertHead(objPos thisPos);
-        void insertTail(objPos thisPos);
-        void removeHead();
-        void removeTail();
-        
+
         void getHeadElement(objPos &returnPos);
         void getTailElement(objPos &returnPos);
         void getElement(objPos &returnPos, int index);
+        
+    //setter   
+        void insertHead(objPos thisPos);
+        void insertTail(objPos thisPos);
+
+        void removeHead();
+        void removeTail();
 };
 
 #endif
